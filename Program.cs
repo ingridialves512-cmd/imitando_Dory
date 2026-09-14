6ddebdc3cc952223a23c2eb645a5a1f09d7ba089
ingridi <ingridialves512@gmail.com>
2026-09-14 18:20:32 -0300

    termino de atividade

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("--- Imitando Dory ---\n");
+
+Console.Write("Digite uma frase...: ");
+string frase = Console.ReadLine()!;
+
+string fraseEmBaleies = frase
+    .Replace("a", "continue a nadar")
+    .Replace("A", "coontinuuee aa naadarr")
+    .Replace("e", "continue a nadar")
+    .Replace("E", "coontinuuee aa naadarr")
+    
+;
+Console.WriteLine($"\nEm baleiês:\n\n{fraseEmBaleies}");
